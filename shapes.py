80c8fe118f7640d75bfdc8d7a6c71f1b0146686d
d6h <hunterjk@ornl.gov>
2023-06-14 14:54:54 -0400

    outline of little plan fo rproject

--- shapes.py
+++ shapes.py
@@ -1,1 +1,7 @@
 print("Hello")
+
+# task 1 - make shape in ESP
+# task 2 - tutorial for how to make shape with code
+# task 3 - make equation for perimeter of shape
+# task 4 - make equation for area of shape
+# task 5 - collect all data and present findings
