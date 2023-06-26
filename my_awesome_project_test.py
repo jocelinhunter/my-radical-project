97c35a137b26909733fa1cbfd57bbbc417b0c58b
d6h <hunterjk@ornl.gov>
2023-06-26 14:22:17 -0400

    finished unit tests

--- my_awesome_project_test.py
+++ my_awesome_project_test.py
@@ -1,6 +1,7 @@
 
 from classdef import *
 import math
+import pytest
 def test_rectangle():
     r1 = Rectangle(4,5,5,5)
     r1a= r1.area()
@@ -13,7 +14,6 @@ def test_rectangle():
     assert r2a == 42
     r2p = r2.perimeter()
     assert r2p == 26
-
 
 def test_circle():
     c1 =  Circle(4,4,4)
@@ -31,7 +31,31 @@ def test_circle():
 def test_Striangle():
     t1 = ScTriangle(5,5,7,8,9)
     t1a = t1.area()
-    assert t1a == 26.83281573
+    assert t1a == pytest.approx(26.83281573)
     t2 = ScTriangle(5,5,6,5,4)
     t2a = t2.area()
-    assert t2a == 9.921567418
+    assert t2a == pytest.approx(9.921567418)
+
+def test_Itriangle():
+    t3 = IsTriangle(5,7,4,4)
+    t3a = t3.area()
+    assert t3a == pytest.approx(16.34587104)
+    t4 = IsTriangle(6,4,3,3)
+    t4a = t4.area()
+    assert t4a == pytest.approx(7.937253933)
+
+def test_Etriangle():
+    t5 = EqTriangle(4,5,5)
+    t5a = t5.area()
+    assert t5a == pytest.approx(6.92820323)
+    t6 = EqTriangle(8,9,2)
+    t6a = t6.area()
+    assert t6a == pytest.approx(27.71281292)
+
+def test_ellipse():
+    e1 = Ellipse(5,8,5,5)
+    e1a = e1.area()
+    assert e1a == pytest.approx(125.66371)
+    e2 = Ellipse(7,4,6,9)
+    e2a = e2.area()
+    assert e2a == pytest.approx(87.96459)
